8fdb429bf1fc1468be939c0bf69b51d5497f81c9
Akshat Arora <akshat.arora456@gmail.com>
2023-05-12 12:18:33 -0400

    s

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,26 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!products) {
+      res
+        .status(404)
+        .json({
+          message: "Producted to be deleted is not found with that id!",
+        });
+      return;
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
